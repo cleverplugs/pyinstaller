cleverplugs/pyinstaller

commit 28b904008247978c17437b264bd7b015f4d478df

Cleanup: use os.path.join() instead of string concat and "/" - PyInstaller part
git-svn-id: http://svn.pyinstaller.org/trunk@1536 8dd32b29-ccff-0310-8a9a-9233e24343b1

PyInstaller/archive.py
@@ -42,6 +42,7 @@ import marshal
 import struct
 import imp
 import sys
+import os
 
 _c_suffixes = filter(lambda x: x[2] == imp.C_EXTENSION, imp.get_suffixes())
 
@@ -372,7 +373,7 @@ class ZlibArchive(Archive):
         else:
             txt = iu._string_replace(txt, '\r\n', '\n')
             try:
-                co = compile(txt, "%s/%s" % (self.path, nm), 'exec')
+                co = compile(txt, os.path.join(self.path, nm), 'exec')
             except SyntaxError, e:
                 print "Syntax error in", pth[:-1]
                 print e.args

PyInstaller/build.py
@@ -110,7 +110,7 @@ def compile_pycos(toc):
 
     # For those modules that need to be rebuilt, use the build directory
     # PyInstaller creates during the build process.
-    basepath = "/".join([BUILDPATH, "localpycos"])
+    basepath = os.path.join(BUILDPATH, "localpycos")
 
     new_toc = []
     for (nm, fnm, typ) in toc:
@@ -138,13 +138,12 @@ def compile_pycos(toc):
                     # as leading path and use "__init__" as the module name
                     leading, mod_name = nm.split("."), "__init__"
 
-                leading.insert(0, basepath)
-                leading = "/".join(leading)
+                leading = os.path.join(basepath, *leading)
 
                 if not os.path.exists(leading):
                     os.makedirs(leading)
 
-                fnm = "/".join([leading, mod_name + ext])
+                fnm = os.path.join(leading, mod_name + ext)
                 py_compile.compile(source_fnm, fnm)
 
         new_toc.append((nm, fnm, typ))

PyInstaller/mf.py
@@ -212,6 +212,7 @@ if zipimport:
             fn = fn.replace(".", "/")
             return BaseDirOwner.getmod(self, fn)
         def _modtime(self, fn):
+            # zipfiles always use forward slashes
             fn = fn.replace("\\","/")
             try:
                 dt = self.zf.getinfo(fn).date_time
@@ -1090,8 +1091,8 @@ def _resolveCtypesImports(cbinaries):
             # "man ld.so" says that we should first search LD_LIBRARY_PATH
             # and then the ldcache
             for d in os.environ["LD_LIBRARY_PATH"].split(":"):
-                if os.path.isfile(d + "/" + cpath):
-                    cpath = d + "/" + cpath
+                if os.path.isfile(os.path.join(d, cpath)):
+                    cpath = os.path.join(d, cpath)
                     break
             else:
                 txt = subprocess.Popen(["ldconfig", "-p"]), stdout=PIPE).communicate()[0]

PyInstaller/utils.py
@@ -32,7 +32,7 @@ def get_svn_revision():
     '''
     rev = 0
     path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    entries_path = '%s/.svn/entries' % path
+    entries_path = os.path.join(path, '.svn', 'entries')
 
     try:
         entries = open(entries_path, 'r').read()

commit 7ac7c176bd53850221f63a28511323851dba9edf

#393 Fix Mac OS X issue in archive.py introduced in r1536
git-svn-id: http://svn.pyinstaller.org/trunk@1553 8dd32b29-ccff-0310-8a9a-9233e24343b1

PyInstaller/archive.py
@@ -42,7 +42,6 @@ import marshal
 import struct
 import imp
 import sys
-import os
 
 _c_suffixes = filter(lambda x: x[2] == imp.C_EXTENSION, imp.get_suffixes())
 
@@ -374,7 +373,7 @@ class ZlibArchive(Archive):
             txt = iu._string_replace(txt, '\r\n', '\n')
             try:
                 import os
-                co = compile(txt, os.path.join(self.path, nm), 'exec')
+                co = compile(txt, self.os.path.join(self.path, nm), 'exec')
             except SyntaxError, e:
                 print "Syntax error in", pth[:-1]
                 print e.args