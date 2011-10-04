cleverplugs/pyinstaller

commit fa48efac34d67efbe6354c5d5bb0d24b81b31d25

Use more string functions in iu.py instead of emulations required by pre-Python 2.0
git-svn-id: http://svn.pyinstaller.org/trunk@1571 8dd32b29-ccff-0310-8a9a-9233e24343b1

PyInstaller/iu.py
@@ -567,29 +567,21 @@ class ImportManager:
 #========= some helper functions =============================#
 
 def packagename(s):
-    for i in range(len(s)-1, -1, -1):
-        if s[i] == '.':
-            break
+    i = s.rfind('.')
+    if i >= 0:
+        return s[:i]
     else:
         return ''
-    return s[:i]
 
 def namesplit(s):
-    rslt = []
-    i = j = 0
-    for j in range(len(s)):
-        if s[j] == '.':
-            rslt.append(s[i:j])
-            i = j+1
-    if i < len(s):
-        rslt.append(s[i:])
-    return rslt
+    return s.split('.')
 
 def getpathext(fnm):
-    for i in range(len(fnm)-1, -1, -1):
-        if fnm[i] == '.':
-            return fnm[i:]
-    return ''
+    i = s.rfind('.')
+    if i >= 0:
+        return s[i:]
+    else:
+        return ''
 
 def pathisdir(pathname):
     "Local replacement for os.path.isdir()."

commit 21dfe134dafbe538c3383e0aca3af6ae9db67261

#431 Fix regression introduced in r1571 which prevents some modules to not work
git-svn-id: http://svn.pyinstaller.org/trunk@1609 8dd32b29-ccff-0310-8a9a-9233e24343b1

PyInstaller/iu.py
@@ -567,6 +567,11 @@ class ImportManager:
 #========= some helper functions =============================#
 
 def packagename(s):
+    """
+    For package name like 'module.submodule.subsubmodule' returns
+    'module.submodule'. If name does not contain any dots '.',
+    empty string '' is returned.
+    """
     i = s.rfind('.')
     if i >= 0:
         return s[:i]
@@ -574,7 +579,19 @@ def packagename(s):
         return ''
 
 def namesplit(s):
-    return s.split('.')
+    """
+    Split package name at the position of dot '.'.
+
+    Examples:
+        'module.submodule' =>  ['module', 'submodule']
+        'module'           =>  ['module']
+        ''                 =>  []
+    """
+    rslt = []
+    # Ensure that for empty string '' an empty list is returned.
+    if s:
+        rslt = s.split('.')
+    return rslt
 
 def getpathext(fnm):
     i = s.rfind('.')