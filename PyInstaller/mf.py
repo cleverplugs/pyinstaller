cleverplugs/pyinstaller

commit 49833751190fd58b0a67ca689c4c0c70f6704264

Cleanup: Move handling of hooks into a method by its own.
git-svn-id: http://svn.pyinstaller.org/trunk@1757 8dd32b29-ccff-0310-8a9a-9233e24343b1

PyInstaller/mf.py
@@ -635,34 +635,7 @@ class ImportTracker:
             except AttributeError:
                 pass
             else:
-                # rearranged so that hook() has a chance to mess with hiddenimports & attrs
-                if hasattr(hook, 'hook'):
-                    mod = hook.hook(mod)
-                if hasattr(hook, 'hiddenimports'):
-                    for impnm in hook.hiddenimports:
-                        mod.imports.append((impnm, 0, 0, -1))
-                if hasattr(hook, 'attrs'):
-                    for attr, val in hook.attrs:
-                        setattr(mod, attr, val)
-                if hasattr(hook, 'datas'):
-                    # hook.datas is a list of globs of files or
-                    # directories to bundle as datafiles. For each
-                    # glob, a destination directory is specified.
-                    def _visit((base, dest_dir, datas), dirname, names):
-                        for fn in names:
-                            fn = os.path.join(dirname, fn)
-                            if os.path.isfile(fn):
-                                datas.append((dest_dir + fn[len(base)+1:], fn, 'DATA'))
-
-                    datas = mod.datas # shortcut
-                    for g, dest_dir in hook.datas:
-                        if dest_dir: dest_dir += os.sep
-                        for fn in glob.glob(g):
-                            if os.path.isfile(fn):
-                                datas.append((dest_dir + os.path.basename(fn), fn, 'DATA'))
-                            else:
-                                os.path.walk(fn, _visit,
-                                             (os.path.dirname(fn), dest_dir, datas))
+                self.handle_hook(mod, hook)
                 if fqname != mod.__name__:
                     logger.warn("%s is changing it's name to %s",
                                 fqname, mod.__name__)
@@ -674,6 +647,37 @@ class ImportTracker:
         # self.modules[fqname] = mod
         # here
         return mod
+
+
+    def handle_hook(self, mod, hook):
+        if hasattr(hook, 'hook'):
+            mod = hook.hook(mod)
+        if hasattr(hook, 'hiddenimports'):
+            for impnm in hook.hiddenimports:
+                mod.imports.append((impnm, 0, 0, -1))
+        if hasattr(hook, 'attrs'):
+            for attr, val in hook.attrs:
+                setattr(mod, attr, val)
+        if hasattr(hook, 'datas'):
+            # hook.datas is a list of globs of files or
+            # directories to bundle as datafiles. For each
+            # glob, a destination directory is specified.
+            def _visit((base, dest_dir, datas), dirname, names):
+                for fn in names:
+                    fn = os.path.join(dirname, fn)
+                    if os.path.isfile(fn):
+                        datas.append((dest_dir + fn[len(base)+1:], fn, 'DATA'))
+
+            datas = mod.datas # shortcut
+            for g, dest_dir in hook.datas:
+                if dest_dir: dest_dir += os.sep
+                for fn in glob.glob(g):
+                    if os.path.isfile(fn):
+                        datas.append((dest_dir + os.path.basename(fn), fn, 'DATA'))
+                    else:
+                        os.path.walk(fn, _visit,
+                                     (os.path.dirname(fn), dest_dir, datas))
+        
 
     def getwarnings(self):
         warnings = self.warnings.keys()

commit 548fcb40078d55d91939a755fecccaf8f38d0b3f

fix win32 regression introduced in r1757
git-svn-id: http://svn.pyinstaller.org/trunk@1803 8dd32b29-ccff-0310-8a9a-9233e24343b1

PyInstaller/mf.py
@@ -634,7 +634,7 @@ class ImportTracker:
             except AttributeError:
                 pass
             else:
-                self.handle_hook(mod, hook)
+                mod = self._handle_hook(mod, hook)
                 if fqname != mod.__name__:
                     logger.warn("%s is changing it's name to %s",
                                 fqname, mod.__name__)
@@ -648,7 +648,7 @@ class ImportTracker:
         return mod
 
 
-    def handle_hook(self, mod, hook):
+    def _handle_hook(self, mod, hook):
         if hasattr(hook, 'hook'):
             mod = hook.hook(mod)
         if hasattr(hook, 'hiddenimports'):
@@ -676,6 +676,7 @@ class ImportTracker:
                     else:
                         os.path.walk(fn, _visit,
                                      (os.path.dirname(fn), dest_dir, datas))
+        return mod
         
 
     def getwarnings(self):