cleverplugs/pyinstaller

commit b6228f333d42029f65b539a69d65cf63917d3520

Minor cleanup: removed one indention level.
git-svn-id: http://svn.pyinstaller.org/trunk@1756 8dd32b29-ccff-0310-8a9a-9233e24343b1

PyInstaller/build.py
@@ -420,27 +420,28 @@ class Analysis(Target):
         datas = []    # datafiles to bundle
         rthooks = []  # rthooks if needed
         for modnm, mod in importTracker.modules.items():
-            # FIXME: why can we have a mod == None here?
-            if mod is not None:
-                rthooks.extend(_findRTHook(modnm))  # XXX
-                datas.extend(mod.datas)
-                if isinstance(mod, mf.BuiltinModule):
-                    pass
-                elif isinstance(mod, mf.ExtensionModule):
-                    binaries.append((mod.__name__, mod.__file__, 'EXTENSION'))
-                    # allows hooks to specify additional dependency
-                    # on other shared libraries loaded at runtime (by dlopen)
-                    binaries.extend(mod.binaries)
-                elif isinstance(mod, (mf.PkgInZipModule, mf.PyInZipModule)):
-                    zipfiles.append(("eggs/" + os.path.basename(str(mod.owner)),
-                                     str(mod.owner), 'ZIPFILE'))
-                else:
-                    # mf.PyModule instances expose a list of binary
-                    # dependencies, most probably shared libraries accessed
-                    # via ctypes. Add them to the overall required binaries.
-                    binaries.extend(mod.binaries)
-                    if modnm != '__main__':
-                        pure.append((modnm, mod.__file__, 'PYMODULE'))
+            if mod is None:
+                # FIXME: why can we have a mod == None here?
+                continue
+            rthooks.extend(_findRTHook(modnm))  # XXX
+            datas.extend(mod.datas)
+            if isinstance(mod, mf.BuiltinModule):
+                pass
+            elif isinstance(mod, mf.ExtensionModule):
+                binaries.append((mod.__name__, mod.__file__, 'EXTENSION'))
+                # allows hooks to specify additional dependency
+                # on other shared libraries loaded at runtime (by dlopen)
+                binaries.extend(mod.binaries)
+            elif isinstance(mod, (mf.PkgInZipModule, mf.PyInZipModule)):
+                zipfiles.append(("eggs/" + os.path.basename(str(mod.owner)),
+                                 str(mod.owner), 'ZIPFILE'))
+            else:
+                # mf.PyModule instances expose a list of binary
+                # dependencies, most probably shared libraries accessed
+                # via ctypes. Add them to the overall required binaries.
+                binaries.extend(mod.binaries)
+                if modnm != '__main__':
+                    pure.append((modnm, mod.__file__, 'PYMODULE'))
 
         python = config['python']
         if not is_win:

commit d125253edadc9efce0995d4f80924463b541426e

fix win32 regression introduced in r1756
git-svn-id: http://svn.pyinstaller.org/trunk@1802 8dd32b29-ccff-0310-8a9a-9233e24343b1

PyInstaller/build.py
@@ -417,12 +417,16 @@ class Analysis(Target):
         zipfiles = []  # zipfiles to bundle
         datas = []    # datafiles to bundle
         rthooks = []  # rthooks if needed
+
         for modnm, mod in importTracker.modules.items():
+
+            # FIXME: why can we have a mod == None here?
             if mod is None:
-                # FIXME: why can we have a mod == None here?
                 continue
+
             rthooks.extend(_findRTHook(modnm))  # XXX
             datas.extend(mod.datas)
+
             if isinstance(mod, mf.BuiltinModule):
                 pass
             elif isinstance(mod, mf.ExtensionModule):