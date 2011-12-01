cleverplugs/pyinstaller

commit 0ede640a3f40db119e45367bdd87daac2542b4d0

Implement exec_python which return stdout of the invoked command.
git-svn-id: http://svn.pyinstaller.org/trunk@1858 8dd32b29-ccff-0310-8a9a-9233e24343b1

PyInstaller/compat.py
@@ -165,12 +165,7 @@ def exec_command_rc(*cmdargs, **kwargs):
     return subprocess.call(cmdargs, **kwargs)
 
 
-def exec_python_rc(*args, **kwargs):
-    """
-    Wrap running python script in a subprocess.
-
-    Return exit code of the invoked command.
-    """
+def __wrap_python(args, kwargs):
     cmdargs = [sys.executable]
 
     # Mac OS X supports universal binaries (binary for multiple architectures.
@@ -187,6 +182,24 @@ def exec_python_rc(*args, **kwargs):
 
     cmdargs.extend(args)
 
+
+def exec_python(*args, **kwargs):
+    """
+    Wrap running python script in a subprocess.
+
+    Return stdout of the invoked command.
+    """
+    cmdargs, kwargs = __wrap_python(args, kwargs)
+    return exec_command(*cmdargs, **kwargs)
+
+
+def exec_python_rc(*args, **kwargs):
+    """
+    Wrap running python script in a subprocess.
+
+    Return exit code of the invoked command.
+    """
+    cmdargs, kwargs = __wrap_python(args, kwargs)
     return exec_command_rc(*cmdargs, **kwargs)
 
 

commit eb54d6df35cac174856e0c9d6f312eab3f9c7a68

Fixed bug introduced in r1858.
git-svn-id: http://svn.pyinstaller.org/trunk@1859 8dd32b29-ccff-0310-8a9a-9233e24343b1

PyInstaller/compat.py
@@ -181,6 +181,7 @@ def __wrap_python(args, kwargs):
         cmdargs.append(_PYOPTS)
 
     cmdargs.extend(args)
+    return cmdargs, kwargs
 
 
 def exec_python(*args, **kwargs):